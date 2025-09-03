9db28467060847b996bfaacafb78ecd12d228168
Krish4bh <krishabhstudy@gmail.com>
2025-09-04 01:21:24 +0530

    Added addresses table

--- Database/schema.sql
+++ Database/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
     contact_details VARCHAR(25) NOT NULL
 );
 
+
 CREATE TABLE products (
 	product_id INT AUTO_INCREMENT PRIMARY KEY, 
     product_name VARCHAR(150) NOT NULL, 
@@ -15,10 +16,36 @@ CREATE TABLE products (
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
+
 CREATE TABLE cart (
 	cart_id INT AUTO_INCREMENT PRIMARY KEY,
     total_price DECIMAL(10, 2) NOT NULL, 
     product_names VARCHAR(200) NOT NULL, 
+    user_id INT,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+
+CREATE TABLE orders (
+	order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATETIME DEFAULT NOW() NOT NULL, 
+    order_price DECIMAL(10, 2) NOT NULL, 
+    quantity INT NOT NULL, 
+    shipping_address VARCHAR(255) NOT NULL, 
+    user_id INT, 
+    product_id INT,
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
+    FOREIGN KEY(product_id) REFERENCES products(product_id)
+);
+
+
+CREATE TABLE addresses (
+	address_id INT AUTO_INCREMENT PRIMARY KEY, 
+    permanent_address VARCHAR(250) NOT NULL, 
+    pincode INT NOT NULL, 
+    state VARCHAR(20) NOT NULL, 
+    city VARCHAR(20) NOT NULL, 
+    landline INT, 
     user_id INT,
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
