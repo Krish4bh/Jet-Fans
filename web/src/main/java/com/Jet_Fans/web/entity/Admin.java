059085e6ecd64d59cf324b53e835d28c1deaeda6
Krish4bh <krishabhstudy@gmail.com>
2025-09-22 14:36:37 +0530

    Created Admin entity

---
+++ web/src/main/java/com/Jet_Fans/web/entity/Admin.java
@@ -0,0 +1,56 @@
+package com.Jet_Fans.web.entity;
+
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Value;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @Value("querty")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
