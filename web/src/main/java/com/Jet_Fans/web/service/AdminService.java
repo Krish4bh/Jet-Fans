fbe356b55d861da9bae323f6815893c7a7de1c97
Krish4bh <krishabhstudy@gmail.com>
2025-09-22 15:49:28 +0530

    Created Admin service class

---
+++ web/src/main/java/com/Jet_Fans/web/service/AdminService.java
@@ -0,0 +1,58 @@
+package com.Jet_Fans.web.service;
+
+import com.Jet_Fans.web.entity.Admin;
+import com.Jet_Fans.web.repository.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminRepo adminRepo;
+
+    public List<Admin> getAll() {
+        return adminRepo.findAll();
+    }
+
+    public Admin getById(Long id) {
+        return adminRepo.findById(id).orElseThrow(() -> new RuntimeException("Admin with id " + id + "not found."));
+    }
+
+    public void createAdmin(Admin admin) {
+        adminRepo.save(admin);
+    }
+
+    public void updateAdmin(Admin admin) {
+        Admin adminFromDb = adminRepo.findById(admin.getId()).orElseThrow(() -> new RuntimeException("Admin with id " + admin.getId() + "not found."));
+
+        if (admin.getName() != null) {
+            adminFromDb.setName(admin.getName());
+        }
+
+        if (admin.getEmail() != null) {
+            adminFromDb.setEmail(admin.getEmail());
+        }
+
+        if (admin.getPassword() != null) {
+            adminFromDb.setPassword(admin.getPassword());
+        }
+
+        adminRepo.save(adminFromDb);
+    }
+
+    public void deleteById(Long id) {
+        adminRepo.deleteById(id);
+    }
+
+    public boolean verifyAdmin(String email, String password) {
+        Admin admin = adminRepo.findByEmail(email);
+
+        if (admin.getPassword() == password) {
+            return true;
+        }
+        return false;
+    }
+}
