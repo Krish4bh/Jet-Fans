7c4e6139ec5b2a56553d4e7795f6a5a2f9179e93
Krish4bh <krishabhstudy@gmail.com>
2025-09-24 20:41:33 +0530

    Created User Controller

---
+++ web/src/main/java/com/Jet_Fans/web/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.Jet_Fans.web.controller;
+
+
+import com.Jet_Fans.web.entity.User;
+import com.Jet_Fans.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/user/login")
+    private String userLogin(@RequestParam String email, @RequestParam String password, Model model) {
+        if (userService.verifyUser(email, password)) {
+            return "redirect:/user/hero-page";
+        }
+        model.addAttribute("error", "Invalid email or password");
+        return "user-login";
+    }
+
+    @PostMapping("user/register")
+    private String userRegister(@ModelAttribute("user") User user, Model model) {
+        try {
+            userService.createUser(user);
+            return "redirect:/user/hero-page";
+        } catch (RuntimeException e) {
+            model.addAttribute(e.getMessage());
+            return "user-login";
+        }
+    }
+
+    @GetMapping("/user/profile/{id}")
+    private String userProfile(@PathVariable Long id, Model model) {
+        User user = userService.getById(id);
+        model.addAttribute("userDetails", user);
+        return "user-profile";
+    }
+
+    @PostMapping("/user/update-user")
+    private String editUser(User user) {
+        userService.updateUser(user);
+        System.out.println("************************************************************************");
+        System.out.println(user.getName() + " user details has been successfully updated");
+        System.out.println("************************************************************************");
+        return "redirect:/user/user-profile";
+    }
+
+    @PostMapping("/user/delete-user")
+    private String removeUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "user-login";
+    }
+
+}
