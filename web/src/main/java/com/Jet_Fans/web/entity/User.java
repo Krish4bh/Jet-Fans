5eb021f047cb72a7a353183eef3dd8b8344b985f
Krish4bh <krishabhstudy@gmail.com>
2025-09-24 13:43:52 +0530

    Create User entity class

---
+++ web/src/main/java/com/Jet_Fans/web/entity/User.java
@@ -0,0 +1,75 @@
+package com.Jet_Fans.web.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Name", nullable = false)
+    private String name;
+
+    @Column(name = "Email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "Phone_Number", nullable = false)
+    private int phone;
+
+    @Column(name = "Address", nullable = false, columnDefinition = "TEXT")
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "Password", nullable = false)
+    private String password;
+}
