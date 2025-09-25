51bbfb9bdd886dcf9d88e8f3e5cbde2382a516bf
Krish4bh <krishabhstudy@gmail.com>
2025-09-25 14:57:26 +0530

    Created Order Repo class

---
+++ web/src/main/java/com/Jet_Fans/web/repository/OrderRepo.java
@@ -0,0 +1,14 @@
+package com.Jet_Fans.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.Jet_Fans.web.entity.Order;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Long> {
+
+    public List<Order> getAllByUserId(Long userId);
+
+}
