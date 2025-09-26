5fe8523eb15a6ef452f03fa96a045f76b66b0953
Krish4bh <krishabhstudy@gmail.com>
2025-09-26 14:27:04 +0530

    Created Cart entity Class

---
+++ web/src/main/java/com/Jet_Fans/web/entity/Cart.java
@@ -0,0 +1,54 @@
+package com.Jet_Fans.web.entity;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "UpdatedAt")
+    private LocalDateTime updatedAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItem;
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<CartItem> getCartItem() {
+        return cartItem;
+    }
+
+    public void setCartItem(List<CartItem> cartItem) {
+        this.cartItem = cartItem;
+    }
+}
