0ad83ca3f214000d9ebe53615992790b4f7314bb
Krish4bh <krishabhstudy@gmail.com>
2025-09-26 14:46:29 +0530

    Created CartItem entity class

--- web/src/main/java/com/Jet_Fans/web/entity/Cart.java
+++ web/src/main/java/com/Jet_Fans/web/entity/Cart.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "Cart")
+@Table(name = "carts")
 public class Cart {
 
     @Id
---
+++ web/src/main/java/com/Jet_Fans/web/entity/CartItem.java
@@ -0,0 +1,55 @@
+package com.Jet_Fans.web.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "CartItems")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Quantity", nullable = false)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+}
