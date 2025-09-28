7a0935ed814d1e2b636d2da10009f391e7170164
Krish4bh <krishabhstudy@gmail.com>
2025-09-28 21:19:37 +0530

    Home page Shop Now button is working correctly

--- web/src/main/java/com/Jet_Fans/web/controller/HomeController.java
+++ web/src/main/java/com/Jet_Fans/web/controller/HomeController.java
@@ -1,18 +1,25 @@
 package com.Jet_Fans.web.controller;
 
+import com.Jet_Fans.web.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/jet-fans/home")
     public String jetFansHome() {
         return "home";
     }
 
-    @GetMapping("/shop-now")
-    public String shopNowPage() {
+    @GetMapping("/home/shop-now")
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.getAll());
         return "shop-now";
     }
 
