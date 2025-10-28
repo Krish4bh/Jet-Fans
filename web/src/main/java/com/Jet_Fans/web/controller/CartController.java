073247b1b25d9fb9ed04f58087ddf3c278c639d9
Krish4bh <krishabhstudy@gmail.com>
2025-10-28 12:43:32 +0530

    Added show cart mapping

--- web/src/main/java/com/Jet_Fans/web/controller/CartController.java
+++ web/src/main/java/com/Jet_Fans/web/controller/CartController.java
@@ -9,9 +9,13 @@ import com.Jet_Fans.web.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 public class CartController {
@@ -48,4 +52,23 @@ public class CartController {
         redirectAttributes.addFlashAttribute("success", "Product added to your cart!");
         return "redirect:/home/shop-now";
     }
+
+    @GetMapping("/cart")
+    public String showCart(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("loggedInUser");
+        if (user == null) {
+            model.addAttribute("loginMessage", "Please log in to view your cart.");
+            return "UserLogin"; // or redirect to login page
+        }
+
+        Cart cart = cartService.getCartByUserId(user.getId());
+        if (cart != null && cart.getCartItem() != null) {
+            model.addAttribute("cartItems", cart.getCartItem());
+        } else {
+            model.addAttribute("cartItems", List.of());
+        }
+
+        return "fragments :: cartFragment";
+    }
+
 }
