e02d09ddd5aff0bd6993b9257753cde075de28a8
Krish4bh <krishabhstudy@gmail.com>
2025-11-03 23:13:24 +0530

    Added delete by product id interface

--- web/src/main/java/com/Jet_Fans/web/repository/CartItemRepo.java
+++ web/src/main/java/com/Jet_Fans/web/repository/CartItemRepo.java
@@ -1,7 +1,11 @@
 package com.Jet_Fans.web.repository;
 
 import com.Jet_Fans.web.entity.CartItem;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +15,9 @@ public interface CartItemRepo extends JpaRepository<CartItem, Long> {
 
     public List<CartItem> findByCartId(Long cartId);
     Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM CartItem c WHERE c.product.id = :productId")
+    void deleteByProductId(@Param("productId") Long productId);
 }
