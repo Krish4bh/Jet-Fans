41b27fb51060a978db5c35abf92f891ed9983bb1
Krish4bh <krishabhstudy@gmail.com>
2025-11-04 17:41:05 +0530

    Added remove feedback method

--- web/src/main/java/com/Jet_Fans/web/service/FeedbackService.java
+++ web/src/main/java/com/Jet_Fans/web/service/FeedbackService.java
@@ -3,6 +3,9 @@ package com.Jet_Fans.web.service;
 import com.Jet_Fans.web.entity.Feedback;
 import com.Jet_Fans.web.repository.FeedbackRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class FeedbackService {
@@ -15,5 +18,18 @@ public class FeedbackService {
 
     public void save(Feedback feedback) {
         feedbackRepo.save(feedback);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Feedback> getAllFeedbacks() {
+        return feedbackRepo.findAll();
+    }
+
+    public void deleteFeedbackById(Long feedbackId) {
+        try {
+            feedbackRepo.deleteById(feedbackId);
+        } catch (Exception e) {
+            throw new RuntimeException("Error deleting feedback: " + e.getMessage());
+        }
     }
 }
